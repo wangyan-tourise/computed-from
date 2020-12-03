6b450c57a1885aef9d4880ae2be49c4f860d15d1
wangyan <395842085@qq.com>
2020-12-03 14:02:05 +0800

    拖拽和磁吸

--- src/components/GuChengShuang/pages/cixi.vue
+++ src/components/GuChengShuang/pages/cixi.vue
@@ -1,21 +1,30 @@
 <template>
   <div id='app'>
-    <div class="header">
-
-    </div>
     <div class="content">
-      <div class="box">
-        <div class="box1" v-for="(item,index) in boxList" :key="index" @mouseover="mouseOver">
-          <div class="box2" v-if="item.id" @mousedown="mouseDown" @mouseup="mouseUp">
-            <img :src="item.imgSrc" width="50px" height="50px" />
-            <div v-if="item.buttons">
-              <el-button type="text" size="large" v-for="val in item.buttons" :key="val.title">{{val.title}}</el-button>
+      <div class="box" @mouseup.capture="mouseUp" @mousemove="mouseMove">
+        <el-carousel indicator-position="outside" :height="pageHeight" :autoplay="false" @change="hadleChange">
+          <el-carousel-item v-for="item in pages" :key="item">
+            <div v-for="(i,index) in boxShowList" :key="index" class="ul">
+              <div class="box1" v-for="val in i" :key="val.id">
+                <div class="box2" :style="`background:${val.backgroundColor}`" @mousedown.stop="mouseDown">
+                  <div class="box3" v-if="val.imgSrc">
+                    <img src="./backImg/tou1.jpg" alt="" width="50px" height="50px">
+                  </div>
+                  <div class="box3" v-if="val.button">
+                    <span>编辑</span>|<span>删除</span>
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
-          <div class="box2" v-else>
-            <div></div>
-          </div>
-        </div>
+            <div v-if="moving" class="box1" :style="`z-index:2;position:absolute;top:${top}px;left:${left}px`">
+              <div class="box2" :style="`background:${topColor}`">
+                <div>
+                </div>
+              </div>
+            </div>
+          </el-carousel-item>
+        </el-carousel>
+
       </div>
     </div>
   </div>
@@ -25,42 +34,106 @@ export default {
   name: 'formate',
   data () {
     return {
-      boxList: [
-        {
-          id: '01',
-          name: '',
-          imgSrc: '../../../assets/image/logo.png',
-          buttons: [
-            {
-              title: '编辑',
-              methods: 'XXX'
-            },
-            {
-              title: '删除',
-              methods: 'XXX'
-            }
-          ]
-        },
-        {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}
-			],
-			indexData:{}
+      boxList: [],
+      boxShowList: [],
+      pageList: [],
+      pages: 4,
+      indexData: {},
+      moving: false,
+      top: 0,
+      left: 0,
+      boxTop: 0,
+      boxLeft: 0,
+      oldX: 0,
+      oldY: 0,
+      bottomColor: 'black',
+      topColor: 'purple',
+      rowCount: 3,
+      columnCount: 8,
+      pageHeight: '360px'
     }
   },
   methods: {
-    mouseOver (e) {
-      console.log(e)
-      // let box1 = document.getElementsByClassName
+    hadleChange (newIndex, oldIndex) {
+      console.log(newIndex, oldIndex)
+      this.pageList[oldIndex] = this.boxShowList
+      this.boxShowList = this.pageList[newIndex]
+    },
+    mouseMove (e) {
+      this.top = e.pageY - this.boxTop - 70
+      this.left = e.pageX - this.boxLeft - 70
+      if (!this.moving) { return }
+      let numX = Math.floor((e.pageX - 70) / 120), numY = Math.floor((e.pageY - 70) / 120)
+      for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 10; j++) {
+          this.boxShowList[i][j].backgroundColor = ''
+        }
+      }
+      console.log(numY, numX)
+      this.boxShowList[numY][numX].backgroundColor = this.bottomColor
     },
     mouseDown (e) {
-      console.log(e)
-
+      this.top = e.pageY - e.offsetY - 70
+      this.left = e.pageX - e.offsetX - 70
+      this.boxTop = e.offsetY
+      this.boxLeft = e.offsetX
+      let numX = Math.floor((e.pageX - 70) / 120), numY = Math.floor((e.pageY - 70) / 120)
+      this.indexData = this.boxShowList[numY][numX]
+      let id = this.boxShowList[numY][numX].id
+      this.boxShowList[numY][numX] = { 'id': id, backgroundColor: this.bottomColor }
+      this.oldX = numX
+      this.oldY = numY
+      this.moving = true
     },
-    mouseUp () {
-
-    }
-  },
+    mouseUp (e) {
+      console.log('抬起')
+      let numX = Math.floor((e.pageX - 70) / 120), numY = Math.floor((e.pageY - 70) / 120)
+      if ((1 <= this.top <= 480) && (1 <= this.left <= 1080)) {
+        let id = this.boxShowList[numY][numX].id
+        this.boxShowList[numY][numX] = this.indexData
+        this.boxShowList[numY][numX].id = id
+      } else {
+        this.boxShowList[this.oldY][this.oldX] = this.indexData
+      }
+      this.boxShowList[numY][numX].backgroundColor = ''
+      this.moving = false
+    },
+	},
+	created(){
+		
+	},
   mounted () {
-
+    for (let i = 0; i < 103; i++) {
+      this.boxList.push({ id: i, backgroundColor: '' })
+    }
+    this.boxList.unshift({ id: 'xi', backgroundColor: '', imgSrc: true, button: true })
+  },
+  watch: {
+    boxList (newVal) {
+      // console.log(newVal)
+      let arr1 = [], arr2 = []
+      for (let i = 0; i < newVal.length; i++) {
+        if (arr2.length === this.rowCount) {
+          this.pageList.push(arr2)
+          arr2 = []
+        }
+        console.log(this.rowCount, this.columnCount, arr2.length, arr1, arr2)
+        if (arr1.length < 10) {
+          arr1.push(newVal[i])
+        } else if (arr1.length === this.columnCount) {
+          arr2.push(arr1)
+          arr1 = []
+          arr1.push(newVal[i])
+        }
+      }
+      arr2.push(arr1)
+      this.pageList.push(arr2)
+      arr1 = []
+      arr2 = []
+      console.log(this.pageList.length)
+      this.pages = this.pageList.length
+      this.boxShowList = this.pageList[0]
+    }
   }
 }
 </script>
@@ -70,31 +143,21 @@ export default {
   height: 100%;
   background-color: purple;
 }
-.header {
-  height: 50px;
-  width: 100%;
-  opacity: 0.5;
-  background-color: white;
-}
 .content {
   width: 100%;
-  height: 645px;
+  height: 100%;
 }
 .box {
-  width: 1200px;
-  height: 600px;
+  width: 100%;
+  height: 100%;
   background: #999;
   opacity: 0.5;
-  position: relative;
-  left: 70px;
-  top: 20px;
+  border-radius: 10px;
+  margin: 0;
 }
 .box1 {
   width: 120px;
   height: 120px;
-  background: #999;
-  /* position: absolute; */
-  display: inline-block;
   padding: 10px;
   box-sizing: border-box;
   cursor: move;
@@ -106,12 +169,28 @@ export default {
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  color: #999;
-}
-.is-active {
-  background: #334;
-  border: 1px solid black;
   border-radius: 10px;
-  color: white;
+  color: #999;
+  opacity: 1;
+}
+.ul {
+  height: 120px;
+  display: flex;
+  flex-direction: row;
+}
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
+}
+
+.el-carousel__item:nth-child(2n) {
+  background-color: #99a9bf;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
 }
 </style>
